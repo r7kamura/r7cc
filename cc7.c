fd2992a6a9c75e48b48d7526dac263c701e9ae78
r7kamura <r7kamura@gmail.com>
2019-10-28 12:39:40 +0900

    Refactor functions order

--- cc7.c
+++ cc7.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Type definitions
+
 typedef enum {
   NODE_TYPE_ADD,
   NODE_TYPE_SUBTRACT,
@@ -22,7 +24,6 @@ struct Node {
   int value;
 };
 
-// Enum to categorize tokens.
 typedef enum {
   TOKEN_TYPE_RESERVED_SYMBOL,
   TOKEN_TYPE_NUMBER,
@@ -38,9 +39,7 @@ struct Token {
   char *string;
 };
 
-Node *generate_branch_node(NodeType, Node *, Node *);
-
-Node *generate_leaf_node(int);
+// Proto-types for recursive functions
 
 Node *expression();
 
@@ -50,56 +49,104 @@ Node *unary();
 
 Node *primary();
 
-void expect(char);
-
-int expect_number();
-
-bool consume(char);
-
-void report_error(char *, char *, ...);
-
-Token *generate_token(TokenType, Token *, char *);
-
-Token *tokenize();
-
-bool at_eof();
-
-void generate_code(Node *);
+// Global variables
 
 Token *token;
 
 char *user_input;
 
-void generate_code(Node *node) {
-  if (node->type == NODE_TYPE_NUMBER) {
-    printf("  push %d\n", node->value);
-    return;
-  }
+// Utility functions
 
-  generate_code(node->lhs);
-  generate_code(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-
-  switch (node->type) {
-  case NODE_TYPE_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case NODE_TYPE_SUBTRACT:
-    printf("  sub rax, rdi\n");
-    break;
-  case NODE_TYPE_MULTIPLY:
-    printf("  imul rax, rdi\n");
-    break;
-  case NODE_TYPE_DIVIDE:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
-  }
-
-  printf("  push rax\n");
+void raise_error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
 }
+
+void report_error(char *location, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int position = location - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", position, "");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// Tokenizer functions
+
+Token *generate_token(TokenType type, Token *current, char *string) {
+  Token *token = calloc(1, sizeof(Token));
+  token->type = type;
+  token->string = string;
+  current->next = token;
+  return token;
+}
+
+Token *tokenize() {
+  char *p = user_input;
+
+  Token head;
+  head.next = NULL;
+  Token *current = &head;
+
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+    } else if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
+      current = generate_token(TOKEN_TYPE_RESERVED_SYMBOL, current, p);
+      p++;
+    } else if (isdigit(*p)) {
+      current = generate_token(TOKEN_TYPE_NUMBER, current, p);
+      current->value = strtol(p, &p, 10);
+    } else {
+      report_error(p, "Expected a number.");
+    }
+  }
+
+  generate_token(TOKEN_TYPE_EOF, current, p);
+  return head.next;
+}
+
+// Token consumer functions
+
+bool consume(char character) {
+  if (token->type == TOKEN_TYPE_RESERVED_SYMBOL &&
+      token->string[0] == character) {
+    token = token->next;
+    return true;
+  }
+  return false;
+}
+
+bool at_eof() {
+  return token->type == TOKEN_TYPE_EOF;
+}
+
+void expect(char expected_character) {
+  if (token->type == TOKEN_TYPE_RESERVED_SYMBOL && token->string[0] == expected_character) {
+    token = token->next;
+  } else {
+    report_error(token->string, "Expected '%c'", expected_character);
+  }
+}
+
+int expect_number() {
+  if (token->type != TOKEN_TYPE_NUMBER) {
+    report_error(token->string, "Expected a number.");
+  }
+
+  int value = token->value;
+  token = token->next;
+  return value;
+}
+
+// AST builder functions
 
 Node *generate_branch_node(NodeType type, Node *lhs, Node *rhs) {
   Node *node = calloc(1, sizeof(Node));
@@ -115,6 +162,8 @@ Node *generate_leaf_node(int value) {
   node->value = value;
   return node;
 }
+
+// eBNF parts functions
 
 // expression = multiply_or_devide ("+" multiply_or_devide | "-" multiply_or_devide)*
 Node *expression() {
@@ -170,93 +219,37 @@ Node *primary() {
   }
 }
 
-void report_error(char *location, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
+// Code generator functions
 
-  int position = location - user_input;
-  fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", position, "");
-  fprintf(stderr, "^ ");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-Token *generate_token(TokenType type, Token *current, char *string) {
-  Token *token = calloc(1, sizeof(Token));
-  token->type = type;
-  token->string = string;
-  current->next = token;
-  return token;
-}
-
-Token *tokenize() {
-  char *p = user_input;
-
-  Token head;
-  head.next = NULL;
-  Token *current = &head;
-
-  while (*p) {
-    if (isspace(*p)) {
-      p++;
-    } else if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
-      current = generate_token(TOKEN_TYPE_RESERVED_SYMBOL, current, p);
-      p++;
-    } else if (isdigit(*p)) {
-      current = generate_token(TOKEN_TYPE_NUMBER, current, p);
-      current->value = strtol(p, &p, 10);
-    } else {
-      report_error(p, "Expected a number.");
-    }
+void generate_code(Node *node) {
+  if (node->type == NODE_TYPE_NUMBER) {
+    printf("  push %d\n", node->value);
+    return;
   }
 
-  generate_token(TOKEN_TYPE_EOF, current, p);
-  return head.next;
-}
+  generate_code(node->lhs);
+  generate_code(node->rhs);
 
-void raise_error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
 
-bool consume(char character) {
-  if (token->type == TOKEN_TYPE_RESERVED_SYMBOL &&
-      token->string[0] == character) {
-    token = token->next;
-    return true;
-  }
-  return false;
-}
-
-bool at_eof() {
-  return token->type == TOKEN_TYPE_EOF;
-}
-
-// If current token is expected, step to the next token.
-// Otherwise raise an error.
-void expect(char expected_character) {
-  if (token->type == TOKEN_TYPE_RESERVED_SYMBOL && token->string[0] == expected_character) {
-    token = token->next;
-  } else {
-    report_error(token->string, "Expected '%c'", expected_character);
-  }
-}
-
-// If current token is a number, return it.
-// Otherwise raise an error.
-int expect_number() {
-  if (token->type != TOKEN_TYPE_NUMBER) {
-    report_error(token->string, "Expected a number.");
+  switch (node->type) {
+  case NODE_TYPE_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case NODE_TYPE_SUBTRACT:
+    printf("  sub rax, rdi\n");
+    break;
+  case NODE_TYPE_MULTIPLY:
+    printf("  imul rax, rdi\n");
+    break;
+  case NODE_TYPE_DIVIDE:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
   }
 
-  int value = token->value;
-  token = token->next;
-  return value;
+  printf("  push rax\n");
 }
 
 int main(int argc, char **argv) {
